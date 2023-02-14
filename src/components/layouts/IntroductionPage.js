73ed1c0822239a09cbf8adbd88c50023a303c132
Shivani Rana <shivrana521@gmail.com>
2023-02-14 14:58:10 +0530

    Pushing all the files

---
+++ src/components/layouts/IntroductionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function IntroductionPage() {
+  return (
+    <div>
+<Header></Header>
+    </div>
+  )
+}
+
+export default IntroductionPage
